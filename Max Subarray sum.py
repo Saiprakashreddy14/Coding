8b7b9375a7bcfcdc499bb9523ae156c0eb73c34d
Sai Prakash reddy <47910930+Saiprakashreddy14@users.noreply.github.com>
2020-05-03 22:58:13 +0530

    Add files via upload

---
+++ Max Subarray sum.py
@@ -0,0 +1,10 @@
+lis = [-2, -3, 4, -1, -2, 1, 5, -3]
+
+def maxsubarraysum(lis):
+    cur_sum = lis[0]
+    max_sum = lis[0]
+    for i in lis[1:]:
+        cur_sum=max(i,cur_sum+i)
+        max_sum=max(max_sum,cur_sum)
+    return max_sum
+print("Max Sum is : ",maxsubarraysum(lis))
